bfaa8c984aa28b0a316cfae6f6ea578401a68f94
emmanueldavids <emmanueldavids417@gmail.com>
2023-10-31 14:29:41 +0000

    added files

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -2,20 +2,20 @@
 
 // resolving the promise using two atributes for the resolve
 
+"use strict";
 
-function getFullResponseFromAPI(success) {
-  return new Promise((resolve, reject) => {
-    if (success){
-      resolve( {
-        'status': 200,
-        'body': 'Success',
-      });
+function getFullResponseFromAPI(success){
+    return new Promise((resolve, reject) => {
+        if (success){
+            resolve({
+                'status': 200,
+                'body': 'Success'
+            });
 
-    } else {
-
-        reject('The fake API is not working currently');
-    }
-
+        } else {
+            reject('The fake API is not working currently');
+        }
+       
     });
 }
 
