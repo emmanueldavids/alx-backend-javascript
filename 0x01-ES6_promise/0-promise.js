ec8d93225ab4ea6a1da49a2e4a438d8c03331fa7
emmanueldavids <emmanueldavids417@gmail.com>
2023-11-01 19:40:05 +0000

    added files

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,3 +1,14 @@
-export default function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {});
+function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+    // Perform some asynchronous API request or other task here
+    // If the task is successful, call resolve with the result
+    // If there's an error, call reject with the error
+
+    // For example, you can use a setTimeout to simulate an async task
+    setTimeout(() => {
+      resolve("API response data");
+    }, 2000); // Simulating a 2-second delay
+  });
 }
+
+export default getResponseFromAPI;
