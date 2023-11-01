5096bec8eb0dc0b3024d963bcb733fa7c480a9f6
emmanueldavids <emmanueldavids417@gmail.com>
2023-11-01 21:11:37 +0000

    added files

--- 0x01-ES6_promise/5-photo-reject.js
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -2,6 +2,5 @@
 
 // 5-photo-reject.js
 export default function uploadPhoto(filename) {
-    return Promise.reject(new Error(`${filename} cannot be processed`));
+  return Promise.reject(new Error(`${filename} cannot be processed`));
 }
-  
