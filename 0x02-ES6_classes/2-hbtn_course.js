9d41f8252a939b990d8b3e975b629c34744c4e00
emmanueldavids <emmanueldavids417@gmail.com>
2023-11-01 23:31:49 +0000

    added files

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
 export default class HolbertonCourse {
-  constructor(name, length, students){
-    if (typeof name != 'string' || typeof length != 'number' || !Array.isArray(students)){        
+  constructor(name, length, students) {
+    if (typeof name !== 'string' || typeof length != 'number' || !Array.isArray(students)) {
       throw new Error('Invalid attributes. Expected name (string), length (number), and students (array of strings).');
     }
 
@@ -11,34 +11,34 @@ export default class HolbertonCourse {
     this._students = students;
   }
 
-  get name(){
+  get name() {
     return this._name;
   }
 
-  get length(){
+  get length() {
     return this._length;
   }
 
-  get students(){
+  get students() {
     return this.students;
   }
 
-  set name(newName){
-    if (typeof newName != 'string'){
+  set name(newName) {
+    if (typeof newName !== 'string') {
       throw new Error('invalid name. Expected a string.');
     }
     this._name = newName;
   }
 
-  set length(newLength){
-    if (typeof newLength != 'number'){
+  set length(newLength) {
+    if (typeof newLength !== 'number') {
       throw new Error('invalid name. Expected a string.');
     }
     this._length = newLength;
   }
 
-  set students(newStudents){
-    if (!Array.isArray(newStudents)){
+  set students(newStudents) {
+    if (!Array.isArray(newStudents)) {
       throw new Error('invalid name. Expected a string.');
     }
   }
