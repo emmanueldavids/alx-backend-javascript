f23ddbebb9f460339a44016d1a5a25c05fbd53f1
emmanueldavids <emmanueldavids417@gmail.com>
2023-11-01 22:48:15 +0000

    added files

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,11 +4,15 @@ import uploadPhoto from './5-photo-reject';
 export default async function handleProfileSignup(
   firstName,
   lastName,
-  fileName,
+  fileName
 ) {
   return Promise.allSettled([
     signUpUser(firstName, lastName),
     uploadPhoto(fileName),
-  ]).then((res) => res.map((o) => ({ status: o.status, value: o.status === 'fulfilled' ? o.value : String(o.reason) }
-  )));
+  ]).then((res) =>
+    res.map((o) => ({
+      status: o.status,
+      value: o.status === 'fulfilled' ? o.value : String(o.reason),
+    }))
+  );
 }
