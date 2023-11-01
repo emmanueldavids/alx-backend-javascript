d1fcec924ba8f4416f663e3bc25dfca65a8c2ea8
emmanueldavids <emmanueldavids417@gmail.com>
2023-11-01 20:50:23 +0000

    added files

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -4,19 +4,19 @@
 
 "use strict";
 
-function handleResponseFromAPI(promise){
-    return promise 
-        .then((data) => {
-            console.log('Got a response from the API');
-            return {
-                'status': 200,
-                'body': 'success'
-            };
-        })
+function handleResponseFromAPI(promise) {
+  return promise 
+    .then((data) => {
+      console.log('Got a response from the API');
+        return {
+          'status': 200,
+          'body': 'success'
+      };
+    })
 
-        .catch((error) => {
-            return new Error();
-        });
+    .catch((error) => {
+      return new Error();
+    });
 
 }
 
