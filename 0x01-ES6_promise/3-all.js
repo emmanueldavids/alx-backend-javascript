a711d896ebe271cec6a240eb7726d61098375cf7
emmanueldavids <emmanueldavids417@gmail.com>
2023-11-01 22:13:14 +0000

    added files

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,21 +1,9 @@
-#!/usr/bin/node
+import { uploadPhoto, createUser } from "./utils";
 
-'use strict';
-
-import {uploadPhoto, createUser} from 'utils.js'
-
-async function handleProfileSignup() {
-    try {
-      const [photoResponse, userResponse] = await Promise.all([
-        uploadPhoto(),
-        createUser(),
-      ]);
-  
-      console.log(`photo: ${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`);
-    } catch (error) {
-      console.log("Signup system offline");
-    }
-  }
-  
-
-export default handleProfileSignup;
+export default function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((res) => {
+      console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`);
+    })
+    .catch(() => console.log("Signup system offline"));
+}
