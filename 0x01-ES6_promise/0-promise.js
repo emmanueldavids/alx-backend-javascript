0da420154cdb168ca5751f08f9c191c6ea810a09
emmanueldavids <emmanueldavids417@gmail.com>
2023-11-01 20:21:20 +0000

    added files

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,18 +1,16 @@
 #!/usr/bin/node
 
-"use strict";
+'use strict';
 
 // This is script returns a promise using function getResponseFromAPI()
 
 function getResponseFromAPI() {
-
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve('This is the response from the API');
-        }, 1000);
-        
-    });
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+    resolve('This is the response from the API');
+    }, 1000);
+    
+  });
 }
 
 export default getResponseFromAPI;
-
