df6f5f339c2a88f5c744ce0c788a772d34b55e30
emmanueldavids <emmanueldavids417@gmail.com>
2023-11-01 23:14:15 +0000

    added files

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
 export default class ClassRoom {
-  constructor(maxStudentsSize){
+  constructor(maxStudentsSize) {
     this._maxStudentsSize = maxStudentsSize;
   }
 
-  maxStudentsSize(){
+  maxStudentsSize() {
     return this._maxStudentsSize;
   }
 }
