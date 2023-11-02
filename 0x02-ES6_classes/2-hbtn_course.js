71bd95a17cad7a59d228bdf503df716fca887cd6
emmanueldavids <emmanueldavids417@gmail.com>
2023-11-02 14:41:23 +0000

    added files

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,46 +1,46 @@
 #!/usr/bin/node
 
 export default class HolbertonCourse {
-  constructor(name, length, students) {
-    if (typeof name !== 'string' || typeof length !== 'number' || !Array.isArray(students)) {
-      throw new Error('Invalid attributes. Expected name (string), length (number), and students (array of strings).');
+    constructor(name, length, students){
+        if (typeof name != 'string' || typeof length != 'number' || !Array.isArray(students)){
+            
+            throw new Error('Invalid attributes. Expected name (string), length (number), and students (array of strings).');
+        }
+        
+        this._name = name;
+        this._length = length;
+        this._students = students;
     }
 
-    this._name = name;
-    this._length = length;
-    this._students = students;
-  }
-
-  get name() {
-    return this._name;
-  }
-
-  get length() {
-    return this._length;
-  }
-
-  get students() {
-    return this.students;
-  }
-
-  set name(newName) {
-    if (typeof newName !== 'string') {
-      throw new Error('invalid name. Expected a string.');
+    get name(){
+        return this._name;
     }
-    this._name = newName;
-  }
 
-  set length(newLength) {
-    if (typeof newLength !== 'number') {
-      throw new Error('invalid name. Expected a string.');
+    get length(){
+        return this._length;
     }
-    this._length = newLength;
-  }
 
-  set students(newStudents) {
-    if (!Array.isArray(newStudents)) {
-      throw new Error('invalid name. Expected a string.');
+    get students(){
+        return this.students;
     }
-    this._students = newStudents;
-  }
+
+    set name(newName){
+        if (typeof newName != 'string'){
+            throw new Error('invalid name. Expected a string.');
+        }
+        this._name = newName;
+    }
+
+    set length(newLength){
+        if (typeof newLength != 'number'){
+            throw new Error('invalid name. Expected a string.');
+        }
+        this._length = newLength;
+    }
+
+    set students(newStudents){
+        if (!Array.isArray(newStudents)){
+            throw new Error('invalid name. Expected a string.');
+        }
+    }
 }
