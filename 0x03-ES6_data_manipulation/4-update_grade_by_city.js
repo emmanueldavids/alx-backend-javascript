024e7aa1b602d126ac9313a0e1c8c16238840f26
emmanueldavids <emmanueldavids417@gmail.com>
2023-11-03 15:30:45 +0000

    added files

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -5,12 +5,12 @@ export default function updateStudentGradeByCity(students, city, newGrades) {
 
   // Use map to update their grades from the newGrades array
   const updatedStudents = studentsInCity.map((student) => {
-  const matchingGrade = newGrades.find((gradeObj) => gradeObj.studentId === student.id);
-  if (matchingGrade) {
-    return { ...student, grade: matchingGrade.grade };
+    const matchingGrade = newGrades.find((gradeObj) => gradeObj.studentId === student.id);
+    if (matchingGrade) {
+      return { ...student, grade: matchingGrade.grade };
   } else {
-    return { ...student, grade: 'N/A' };
-  }
+      return { ...student, grade: 'N/A' };
+    }
   });
 
   return updatedStudents;
