58c1965d42a4c8e50e4ace202e0ff1b47e774cd0
emmanueldavids <emmanueldavids417@gmail.com>
2023-11-03 15:18:33 +0000

    added files

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
-export default function getListStudentIds(arr){
-    if (!Array.isArray(arr)){
-        return [];
-    }
+export default function getListStudentIds(arr) {
+  if (!Array.isArray(arr)) {
+    return [];
+  }
 
-    const ids = arr.map((students) => students.id);
-    return ids;
+  const ids = arr.map((students) => students.id);
+  return ids;
 }
