20dc620f218c7362169d32d080a5f72944fc99fd
emmanueldavids <emmanueldavids417@gmail.com>
2023-11-03 15:54:37 +0000

    added files

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -4,12 +4,12 @@ function updateUniqueItems(groceriesMap) {
   if (!(groceriesMap instanceof Map)) {
     throw new Error('Cannot process');
   }
-  
+
   groceriesMap.forEach((quantity, item) => {
     if (quantity === 1) {
       groceriesMap.set(item, 100);
     }
   });
 }
-  
+
 export default updateUniqueItems;
