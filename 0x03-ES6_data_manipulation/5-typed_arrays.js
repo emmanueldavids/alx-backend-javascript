000f582c4ec232fcc555e862acd78cdfb9ff7e82
emmanueldavids <emmanueldavids417@gmail.com>
2023-11-03 15:28:50 +0000

    added files

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,15 +1,15 @@
 #!/usr/bin/node
 
-export default function createInt8TypedArray(length, position, value){
-    if (position >= length) {
-        throw new Error('Position outside range');
-    }
+export default function createInt8TypedArray(length, position, value) {
+  if (position >= length) {
+    throw new Error('Position outside range');
+  }
 
-    const buffer = new ArrayBuffer(length);
+  const buffer = new ArrayBuffer(length);
 
-    // Use a DataView to set the Int8 value at the specified position
-    const dataView = new DataView(buffer);
-    dataView.setInt8(position, value);
+  // Use a DataView to set the Int8 value at the specified position
+  const dataView = new DataView(buffer);
+  dataView.setInt8(position, value);
 
-    return dataView;
+  return dataView;
 }
